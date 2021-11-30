c5261f812b9182e2523e1889c319cdeb5e9b5593
Belen Valcarce-Perez <belen.valcarce-perez@ericsson.com>
2021-11-30 12:04:00 +0100

    Add buttons

--- src/App.js
+++ src/App.js
@@ -1,58 +1,99 @@
-import React from 'react';
-import logo from './logo.svg';
-import { Counter } from './features/counter/Counter';
-import './App.css';
+import * as React from "react";
+import { Routes, Route } from "react-router-dom";
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+const App = () => {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Counter />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <span>
-          <span>Learn </span>
-          <a
-            className="App-link"
-            href="https://reactjs.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux
-          </a>
-          <span>, </span>
-          <a
-            className="App-link"
-            href="https://redux-toolkit.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Redux Toolkit
-          </a>
-          ,<span> and </span>
-          <a
-            className="App-link"
-            href="https://react-redux.js.org/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            React Redux
-          </a>
-        </span>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/button3" element={<Button3 />}></Route>
+          <Route path="/button4" element={<Button4 />}></Route>
+        </Routes>
       </header>
     </div>
   );
+};
+
+const Home = () => {
+  return (
+    <div>
+      <p>
+        <button class="button button-s" onClick={onClickButton1} type="button">
+          Button 1
+        </button>
+        <button class="button button-s" onClick={onClickButton2} type="button">
+          Button 2
+        </button>
+        <button class="button button-s" onClick={onClickButton3} type="button">
+          Button 3
+        </button>
+      </p>
+    </div>
+  );
+};
+
+function Button3() {
+  console.log("button3");
+  return (
+    <div>
+      <button class="button button-s" onClick={onClickButton4} type="button">
+        Button 4
+      </button>
+      <button class="button button-s" onClick={onClickButton5} type="button">
+        Button 5
+      </button>
+    </div>
+  );
 }
+
+function Button4() {
+  console.log("button4");
+  return (
+    <div>
+      <button class="button button-s" onClick={onClickFinal} type="button">
+        Button Final 1
+      </button>
+      <button class="button button-s" onClick={onClickFinal} type="button">
+        Button Final 2
+      </button>
+    </div>
+  );
+}
+
+const onClickButton1 = () => {
+  console.log("button1");
+  return window.open("https://www.ericsson.com", "_blank");
+};
+
+const onClickButton2 = () => {
+  console.log("button2");
+  return window.open("https://www.google.com", "_blank");
+};
+
+const onClickButton3 = () => {
+  console.log("button3");
+  return window.open("/Button3", "_blank");
+};
+const onClickButton4 = () => {
+  console.log("button4");
+  return window.open("/Button4", "_blank");
+};
+
+const onClickButton5 = () => {
+  console.log("button5");
+  return window.open("https://www.github.com", "_blank");
+};
+
+const onClickFinal = () => {
+  console.log("Final");
+  return window.open(
+    "https://www.youtube.com/watch?v=w9km01hvP4w&ab_channel=EuropeVEVO",
+    "_blank"
+  );
+};
 
 export default App;
